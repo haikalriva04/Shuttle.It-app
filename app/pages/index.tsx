8c9a271c4703a86fcc5b388d8bfe90fca6da2397
Raihan Haikalriva <haikalriva@gmail.com>
2025-11-29 11:17:44 +0700

    added new file

---
+++ app/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-rose-500"
+    >
+      <Text>Hello world</Text>
+    </View>
+  );
+}
