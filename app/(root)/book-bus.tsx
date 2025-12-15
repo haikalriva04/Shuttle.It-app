9227df3947d763ac04f464cd28f7910d002fc339
Raihan Haikalriva <haikalriva@gmail.com>
2025-12-15 18:35:47 +0700

    added new file

--- app/(root)/book-bus.tsx
+++ app/(root)/book-bus.tsx
@@ -1,6 +1,8 @@
 import BookLayout from "@/components/BookLayout";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import { icons } from "@/constants";
 import { useLocationStore } from "@/store";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 const BookBus = () => {
     const { userAddress, 
@@ -10,8 +12,28 @@ const BookBus = () => {
      } = useLocationStore();
 
     return( 
-    <BookLayout>
-        <Text className="text-2xl">Booking Bus</Text>
+    <BookLayout title="Booking Bus">
+        <View className="my-3">
+        <Text className="text-lg font-PoppinsSemiBold mb-1">Kampus Asal</Text>
+        <GoogleTextInput 
+        icon={icons.pickupPin} 
+        initialLocation={userAddress!}
+        containerStyle="bg-neutral-100" 
+        textInputBackgroundColor="#f5f5f5"
+        handlePress={(location) => setUserLocation(location)}
+        />
+        </View>
+
+             <View className="my-3">
+        <Text className="text-lg font-PoppinsSemiBold mb-1">Kampus Tujuan</Text>
+        <GoogleTextInput 
+        icon={icons.destination} 
+        initialLocation={destinationAddress!}
+        containerStyle="bg-neutral-100" 
+        textInputBackgroundColor="transparent"
+        handlePress={(location) => setDestinationLocation(location)}
+        />
+        </View>
     </BookLayout>
     );
 };
--- components/BookLayout.tsx
+++ components/BookLayout.tsx
@@ -1,13 +1,45 @@
-import { View } from "react-native";
+import Map from "@/components/Map";
+import { icons } from "@/constants";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { router } from "expo-router";
+import { useRef } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const BookLayout = ({ title, children }: { title: string;
+     children: React.ReactNode;
+    }) => {
+        const bottomSheetRef = useRef<BottomSheet>(null);
 
 
-const BookLayout = ({ children }: { children: React.ReactNode}) => {
     return(
-            <GestureHandler>
-                <View>
-            {children}
+            <GestureHandlerRootView>
+                <View className="flex-1 bg-white">
+                    <View className="flex flex-col h-screen bg-blue-500">
+                        <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+                    <TouchableOpacity onPress={() => router.back()}>
+                        <View className="w-10 h-10 bg-black rounded-full items-center justify-center"> 
+                            <Image source={icons.backArrowLight}
+                            resizeMode="contain" className="w-6 h-6" />
+                        </View>
+                    </TouchableOpacity>
+                        <Text className="text-xl font-PoppinsSemiBold text-white ml-5">
+                            {title || "Kembali"}
+                        </Text>
+
+                        
+                        </View>
+                        <Map />
+                    </View>
+                    <BottomSheet ref={bottomSheetRef} snapPoints={["40%", "85%"]} 
+                    index={0}>
+                        <BottomSheetScrollView style={{flex: 1, padding: 20}}>
+                            {children}
+                        </BottomSheetScrollView>
+                    </BottomSheet>
                 </View>
-            </GestureHandler>
+            </GestureHandlerRootView>
+
 
     );
 };
